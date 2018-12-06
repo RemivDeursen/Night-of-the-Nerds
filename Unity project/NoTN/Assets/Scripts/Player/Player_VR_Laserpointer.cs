45a620aee1ee9af776052c36b8859d0d54052670
Remi van Deursen <cryslo1994@gmail.com>
2018-12-06 18:40:44 +0100

    VR Walk and pickup

---
+++ Unity project/NoTN/Assets/Scripts/Player/Player_VR_Laserpointer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Player_VR_Laserpointer : MonoBehaviour {
+	private SteamVR_TrackedObject trackedObj;
+
+	void Awake()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
