e1435912334c9b896b67e13a3b6d8f97e12bb38e
Remi van Deursen <cryslo1994@gmail.com>
2018-12-20 12:27:14 +0100

    Laserpointer added

---
+++ Unity project/NoTN/Assets/Scripts/Laser/LaserScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour
+{
+    LineRenderer line;
+    // Use this for initialization
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.enabled = false;
+
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.GetKey(KeyCode.C));
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            StopCoroutine("FireLaser");
+            StartCoroutine("FireLaser");
+        }
+    }
+
+    IEnumerator FireLaser()
+    {
+        line.enabled = true;
+
+        while (Input.GetKey(KeyCode.C))
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            line.SetPosition(0, ray.origin);
+			// If the laser hits a surface (Collider) We let the laser end at the collision point. If not we let it go to the max range (100)
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                line.SetPosition(1, hit.point);
+            }
+            else
+            {
+                line.SetPosition(1, ray.GetPoint(100));
+            }
+
+            yield return null;
+        }
+
+        line.enabled = false;
+    }
+}
