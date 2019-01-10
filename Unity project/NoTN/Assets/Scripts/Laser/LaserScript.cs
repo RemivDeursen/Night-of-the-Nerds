6a9703d033118936d37cba0d0b64fad6ce2f3f7e
Remi van Deursen <cryslo1994@gmail.com>
2019-01-10 22:31:55 +0100

    Reflections partially working

--- Unity project/NoTN/Assets/Scripts/Laser/LaserScript.cs
+++ Unity project/NoTN/Assets/Scripts/Laser/LaserScript.cs
@@ -33,12 +33,29 @@ public class LaserScript : MonoBehaviour
         {
             Ray ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
+            List<Mirror> mirrorList = new List<Mirror>();
 
             line.SetPosition(0, ray.origin);
-			// If the laser hits a surface (Collider) We let the laser end at the collision point. If not we let it go to the max range (100)
-            if (Physics.Raycast(ray, out hit, 100))
+            // If the laser hits a surface (Collider) We let the laser end at the collision point. If not we let it go to the max range (100)
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
                 line.SetPosition(1, hit.point);
+                if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Mirror"))
+                {
+                    if (mirrorList.Contains(hit.transform.GetComponent<Mirror>()))
+                    {
+                        line.positionCount = 1 + mirrorList.Count;
+
+                    }
+                    Debug.Log(line.positionCount);
+                    Vector3 pos = Vector3.Reflect(hit.point - this.transform.position, hit.normal);
+                    line.SetPosition(2, pos);
+                }
+                else
+                {
+                    line.positionCount = 2;
+                }
             }
             else
             {
