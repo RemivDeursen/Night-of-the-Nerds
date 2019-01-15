72e5ead80b70758d94728360d23c62723f6330e2
Lyudmil Pashayanov <sytix1@hotmail.com>
2019-01-16 00:29:53 +0100

    cameraAdded + movement to it

---
+++ Unity project/NoTN/Assets/Scripts/Player/MovementCamera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCamera : MonoBehaviour {
+
+    public GameObject securityCamera;
+    private bool noLeft=false;
+    private bool noRight = false;
+    private bool noUp = false;
+    private bool noDown = false;
+    // Update is called once per frame
+    void Update () {
+        
+        if (noLeft == false)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                securityCamera.transform.Rotate(0, -1, 0,Space.World);
+            }
+        }
+        if (noRight == false)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                securityCamera.transform.Rotate(0, 1, 0,Space.World);
+            }
+        }
+       
+        if (securityCamera.transform.rotation.eulerAngles.y < 97)
+        {
+            noLeft = true;
+        }
+        else
+        {
+            noLeft = false;
+        }
+        if (securityCamera.transform.rotation.eulerAngles.y > 170)
+        {
+            noRight = true;
+        }
+        else
+        {
+            noRight = false;
+        }
+       
+
+    }
+}
