0e125482acaa728165b9cfd79f766229c762e3a9
Sander Boelaars <s.boelaars@student.fontys.nl>
2019-01-17 11:28:28 +0100

    Commit: multiplayer camera + turret basic

---
+++ Unity project/NoTN/Assets/PCcontrolls/CustomNetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager
+{
+    private int _connectionCounter = 0;
+
+    // Server callbacks
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        if (_connectionCounter > 1)
+        {
+            //Possibility for spectators;
+            return;
+        }
+        var player = (GameObject)GameObject.Instantiate(spawnPrefabs[_connectionCounter], spawnPrefabs[_connectionCounter].transform.position, Quaternion.identity);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+        _connectionCounter++;
+    }
+}
